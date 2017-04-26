e64b20dfd0b937fb32b77593254fcff8ff087d61
Bruce Wang <bing.wang@xiaohulu.com>
2017-04-26 13:31:38 +0800

    Initial Commit.

---
+++ audioFilesTools.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import eyed3
+
+#Remove logs
+eyed3.log.setLevel("ERROR")
+
+def isMono(filename):
+	audiofile = eyed3.load(filename)
+	return audiofile.info.mode == 'Mono'
+
+def getGenre(filename):
+	print(filename)
+	audiofile = eyed3.load(filename)
+	#No genre
+	if not audiofile.tag.genre:
+		return None
+	else:
+		return audiofile.tag.genre.name.encode('utf-8')
+
+
+
+	
